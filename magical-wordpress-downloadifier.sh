31b4f56ff2e9adc7cd46cbb76d9b5b4b59bb59ea
Simon Fairbairn <simon@line-in.co.uk>
2016-10-08 15:53:58 +0200

    Adding support for optionally downloading just the files or the database

--- magical-wordpress-downloadifier.sh
+++ magical-wordpress-downloadifier.sh
@@ -42,31 +42,24 @@ if [[ $CONFIG == false ]];then
 	exit 1
 fi
 
+source $CONFIG
 
-# Resolve symlinks
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  ROOT="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$ROOT/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-ROOT="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-
-
-echo $ROOT
-exit 
-
-# The year of uploads. To save time, I only download the current year's uploads, but you can specify "all"
-# to download everything in the WordPress uploads folder.
-year=2015
-
-## END OF USER VARIABLES ##
-file=$year
-if [ $year == "all" ] 
-then
-	echo "Downloading all files"
-	file="."
+if [[ ! -n $remote ]]; then
+	echo "ERROR: Remote server not found in config file"
+	exit 1
+else
+	echo "Config file found and loaded"
 fi
+
+
+function getFiles {
+
+	file=$year
+	if [ $year == "all" ] 
+	then
+		echo "Downloading all files"
+		file="."
+	fi
 
 # Zip up the files
 ssh $remote << EOF
@@ -75,27 +68,45 @@ ssh $remote << EOF
 	ls -lah
 EOF
 
-# Download them
-scp $remote:$uploadPath/$year.zip ~/Downloads/$year.zip
-# Clean up after ourselves
-ssh $remote rm $uploadPath/$year.zip
-# Move the zip file to your local install
-mv ~/Downloads/$year.zip "$localPath/$year.zip"
-# Move to the local directory
-cd "$localPath"
-# Unzip the files
-unzip $year.zip
-# Do a dump of the remote database and download it 
-ssh $remote -C -o CompressionLevel=9 mysqldump -u $remoteU --password=$remotePW --add-drop-table $remoteDB > ~/Downloads/$remoteDB.sql
+	# Download them
+	scp $remote:$uploadPath/$year.zip ~/Downloads/$year.zip
+	# Clean up after ourselves
+	ssh $remote rm $uploadPath/$year.zip
+	# Move the zip file to your local install
+	mv ~/Downloads/$year.zip "$localPath/$year.zip"
+	# Move to the local directory
+	cd "$localPath"
+	# Unzip the files
+	unzip $year.zip
+}
 
-# Install the database locally
-mysql -u $localU --password=$localPW $localDB < ~/Downloads/$remoteDB.sql
-# Replace all references to remote images and files to the local ones instead (assumes database prefix of wp)
+function updateDatabase {
+	# Do a dump of the remote database and download it 
+	ssh $remote -C -o CompressionLevel=9 mysqldump -u $remoteU --password=$remotePW --add-drop-table $remoteDB > ~/Downloads/$remoteDB.sql
+	# Install the database locally
+	mysql -u $localU --password=$localPW $localDB < ~/Downloads/$remoteDB.sql
+	# Replace all references to remote images and files to the local ones instead (assumes database prefix of wp)
 mysql -v -t -u $localU --password=$localPW <<QUERY_INPUT
 		UPDATE \`$localDB\`.\`wp_posts\` SET post_content = REPLACE(post_content, 'http://$remoteURL', 'http://$localURL');
 		SELECT ROW_COUNT();
-		UPDATE \`$localDB\`.\`wp_options\` SET option_value = REPLACE(option_value, 'http://$remoteURL', 'http://$localURL');
-		SELECT ROW_COUNT();		
-		UPDATE \`$localDB\`.\`wp_postmeta\` SET meta_value = REPLACE(meta_value, 'http://$remoteURL', 'http://$localURL');
+		UPDATE \`$localDB\`.\`wp_options\` SET option_value = REPLACE(option_value, 'http://$remoteURL', 'http://$localURL') WHERE option_name='siteurl';
+		SELECT ROW_COUNT();
+		UPDATE \`$localDB\`.\`wp_options\` SET option_value = REPLACE(option_value, 'http://$remoteURL', 'http://$localURL') WHERE option_name='home';
+		SELECT ROW_COUNT();				
+		UPDATE \`$localDB\`.\`wp_postmeta\` SET meta_value = REPLACE(meta_value, 'http://$remoteURL', 'http://$localURL') ;
 		SELECT ROW_COUNT();		
 QUERY_INPUT
+}
+
+if [[ "$FILES" == true ]]; then
+	getFiles
+else 
+	echo "Skipping file transfer..."
+fi
+
+
+if [[ "$DATABASE" == true ]]; then
+	updateDatabase
+else
+	echo "Skipping database transfer..."
+fi
